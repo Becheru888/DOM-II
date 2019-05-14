24af8c8ad2920936996706f973a53ee60caf7c3d
Becheru888 <becheru93@gmail.com>
2019-05-14 15:47:30 +0100

    Event mouseover, keydown, wheel, drag/drop, load, focus done!

--- js/index.js
+++ js/index.js
@@ -1,1 +1,52 @@
-// Your code goes here
+
+//Mouse over event
+
+const navi = document.querySelectorAll('.nav-link')
+
+navi.forEach(element => {
+    element.addEventListener('mouseover', function (event) {
+        event.target.style.color = 'red';
+    })
+})
+
+//Down Key event
+const input = document.querySelector("input")
+const logo = document.querySelector(".logo-heading")
+
+input.addEventListener('keydown', downK);
+
+function downK() {
+    logo.textContent = 'NEW LOGO'
+    logo.style.color = 'blue'
+}
+
+//Wheel Event
+
+const busImage = document.querySelector('.sand');
+
+busImage.addEventListener('wheel', doIt);
+
+function doIt() {
+    busImage.style.display = 'none';
+}
+
+// Load Event
+const load = document.querySelector('.loaded')
+
+window.addEventListener('load', loading)
+
+function loading(){
+    load.textContent = 'Page Fully Loaded!'
+}
+
+// Focus Event
+
+const password = document.querySelector('input[type="password"]');
+
+password.addEventListener('focus', (event) => {
+  event.target.style.background = 'pink';    
+});
+
+password.addEventListener('blur', (event) => {
+  event.target.style.background = '';    
+});
